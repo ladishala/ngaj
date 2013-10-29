ec77a99438f3090a985996071915951c13375922
isuf <isufdeliu@gmail.com>
2013-10-29 20:43:50 +0100

    First Commit

---
+++ src/hig/herd/ngaj/MainActivity.java
@@ -0,0 +1,166 @@
+package hig.herd.ngaj;
+
+import org.brickred.socialauth.android.DialogListener;
+import org.brickred.socialauth.android.SocialAuthAdapter;
+import org.brickred.socialauth.android.SocialAuthAdapter.Provider;
+import org.brickred.socialauth.android.SocialAuthError;
+import org.brickred.socialauth.android.SocialAuthListener;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore.Images;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	SocialAuthAdapter adapter;
+	boolean status;
+	LinearLayout bar;
+	AlertDialog.Builder Alert;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		bar = (LinearLayout) findViewById(R.id.linearbar);
+		bar.setBackgroundResource(R.drawable.bar_gradient);
+
+		adapter = new SocialAuthAdapter(new ResponseListener());
+
+		// Add providers
+		adapter.addProvider(Provider.FACEBOOK, R.drawable.facebook);
+		adapter.addProvider(Provider.TWITTER, R.drawable.twitter);
+		adapter.addProvider(Provider.EMAIL, R.drawable.other);
+		adapter.enable(bar);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+   
+	 
+	public Bitmap screenShot(View view) {
+		Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		view.draw(canvas);
+		return bitmap;
+	}
+
+	private void showalert(String arg)
+	{
+		Alert = new AlertDialog.Builder(this);
+		Alert.setTitle("Confirm Share!");
+		Alert.setMessage("Are you sure you want to share your results on "+arg+"?");
+		Alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+				try {
+					adapter.uploadImageAsync("Description", "Name.png",screenShot(bar), 0, new MessageListener());
+				} catch (Exception ex) {
+					// TODO Auto-generated catch block
+				Toast.makeText(MainActivity.this, ex.getMessage(),Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
+		Alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+							
+				
+			}
+		});
+		Alert.show();
+	
+		
+		
+		
+	}
+	
+	private void postonother()
+	{
+
+		String pathofBmp = Images.Media.insertImage(this.getContentResolver(),screenShot(bar),"Screenshot", null);
+	    Uri bmpUri = Uri.parse(pathofBmp);
+		Intent share = new Intent(Intent.ACTION_SEND);
+		share.setType("image/jpeg");
+		share.putExtra(Intent.EXTRA_STREAM, bmpUri);
+		startActivity(Intent.createChooser(share, "Share Image"));
+	}
+	
+	private final class ResponseListener implements DialogListener {
+		public void onComplete(Bundle values) {
+
+			// Get name of provider after authentication
+			final String providerName = values
+					.getString(SocialAuthAdapter.PROVIDER);
+			if(providerName=="facebook"||providerName=="twitter")
+			{	
+				showalert(providerName);
+			}
+			else
+			{
+				postonother();
+			}
+
+		}
+
+		@Override
+		public void onBack() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onCancel() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onError(SocialAuthError arg0) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	private final class MessageListener implements SocialAuthListener<Integer> {
+		@Override
+		public void onExecute(String provider, Integer t) {
+			Integer status = t;
+			if (status.intValue() == 200 || status.intValue() == 201
+					|| status.intValue() == 204)
+				Toast.makeText(MainActivity.this,
+						"Message posted on " + provider, Toast.LENGTH_LONG)
+						.show();
+			else
+				Toast.makeText(MainActivity.this,
+						"Message not posted on" + provider, Toast.LENGTH_LONG)
+						.show();
+		}
+
+		@Override
+		public void onError(SocialAuthError e) {
+
+		}
+	}
+
+}
